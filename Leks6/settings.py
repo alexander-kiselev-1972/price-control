0423f63b27c7b12b14db0f6cc33bc3fd561d2abb
alexander-kiselev-1972 <67494579+alexander-kiselev-1972@users.noreply.github.com>
2022-02-18 09:27:16 +0100

    Update settings.py

--- Leks6/settings.py
+++ Leks6/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w70i5zm#swb97xvxwa#)^w!62z4r*gz0l^m!uz4_!y99!bolty'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
